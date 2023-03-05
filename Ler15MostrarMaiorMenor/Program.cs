9d6554077fac23a7ed98aecd0ad27faa4c6815c1
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-05 02:01:49 -0300

    Recebendo15 e verificando maior e menor usando Funcao

--- Ler15MostrarMaiorMenor/Program.cs
+++ Ler15MostrarMaiorMenor/Program.cs
@@ -3,16 +3,8 @@
 float[] numbers = new float[15];
 float maior, menor;
 
-for (int i = 0; i < 15; i++)
-{
-    Console.Write("Digite um numero para a {0}ª posição: ", i + 1);
-    numbers[i] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-    Console.Clear();
-}
-for(int i = 0;i < 15; i++)
-{
-    Console.Write(", ", numbers[i]);
-}
+PedeDados();
+ImprimeDados();
 
 //maior = numbers.Max();
 //menor = numbers.Min();
@@ -20,18 +12,45 @@ for(int i = 0;i < 15; i++)
 maior = numbers[0];
 menor = numbers[0];
 
-for (int i = 0; i < 15; i++)
+VerificaMaior();
+VerificaMenor();
+MostraMensagem();
+
+void PedeDados()
 {
-    if(maior < numbers[i])
-        maior = numbers[i];
+    for (int i = 0; i < 15; i++)
+    {
+        Console.Write("Digite um numero para a {0}ª posição: ", i + 1);
+        numbers[i] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Clear();
+    }
 }
-
-for (int i = 0; i < 15; i++)
+void ImprimeDados()
 {
-    if (menor > numbers[i])
-        menor = numbers[i];
+    for (int i = 0; i < 15; i++)
+    {
+        Console.Write(", {0}", numbers[i]);
+    }
 }
-
-
-Console.Write("Maior número digitado: {0}\n" +
+void VerificaMaior()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        if (maior < numbers[i])
+            maior = numbers[i];
+    }
+}
+void VerificaMenor()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        if (menor > numbers[i])
+            menor = numbers[i];
+    }
+}
+void MostraMensagem()
+{
+    Console.Write("Maior número digitado: {0}\n" +
     "Menor número digitado: {1}", maior.ToString("F2"), menor.ToString("F2"));
+}
+
